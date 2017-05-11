c32f88b0f67abd5c1d7ef40c493f9e15c691d4aa
jlanio <lanio.morais@gmail.com>
2017-05-10 21:17:05 -0500

    WIP Translaction

--- app/js/geogig_dev/services/Utils.js
+++ app/js/geogig_dev/services/Utils.js
@@ -8,12 +8,13 @@ class Utils {
 	}
 	
 	static geogig (args, name=''){
-		let child = spawn(`${path}\\geogig\\bin\\geogig.bat`, args,{cwd: this.pwd(name)});
-		child.stdout.setEncoding('utf8');
 		let stdoutData = '';
         let stderrData = '';
+		let child = spawn(`${path}\\geogig\\bin\\geogig.bat`, args,{cwd: this.pwd(name)});
+		child.stdout.setEncoding('utf8');
 		child.stdout.on('data', data => {stdoutData += data});
 		child.stderr.on('data', data => {stderrData += data});
+		
 		return new Promise((resolve, reject) => {
 			child.on('close', code => stderrData ? reject(stderrData) : resolve(stdoutData));
 	        child.on('error', err => reject(err));

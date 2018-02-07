ba33890effc00e1b6452f9007187cedfd96365ef
J.Lânio <lanio.morais@gmail.com>
2018-02-07 15:50:08 -0500

    remove function to update remote repos

--- app/js/controllers/DashboardRemote/dashboardRemoteCtrl.js
+++ app/js/controllers/DashboardRemote/dashboardRemoteCtrl.js
@@ -18,48 +18,27 @@ function dashboardRemoteCtrl($uibModal, $http, toaster,$location ){
         windowClass: "animated flipInY"
     });
   };
-    function get (serverAddress, id){
-      console.log(serverAddress, id);
-        // $http.get(`${serverAddress}repos.json`).success(data => {
-        //   toaster.pop({
-        //       type: 'success',
-        //       title: serverAddress,
-        //       body: `${data.repos.repo.length} existing repositories`,
-        //       showCloseButton: true
-        //   });
-        // }).error(data=>{
-        //     toaster.pop({
-        //         type: 'error',
-        //         title: 'Servidor Offline',
-        //         body: serverAddress,
-        //         showCloseButton: true
-        //     });
-        // })
-    }
-    s.remoteUpdateRepos = () => {
-        // s.mydb.infoRepositorios.conectedIn.forEach((conexao, id) => get(conexao.serverAddress, id));
-    }
-    s.deleteConexaoRemote = (key) => {
-        swal({
-            title: 'Your conexao will be deleted, do you want to continue?',
-            text: "You won't be able to revert this!",
-            type: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Yes, delete it!',
-            cancelButtonText: 'No, cancel!',
-            confirmButtonClass: 'btn btn-success',
-            cancelButtonClass: 'btn btn-danger',
-            buttonsStyling: false
-        }).then(() => {
-          storage.remove(key)
-          swal('Deleted!', 'Your conexao has been deleted.','success')
-        }, (dismiss) => {
-        if (dismiss === 'cancel')
-          swal('Cancelled','Your conexao is safe :)','error')
-        })
-    }
+  s.deleteConexaoRemote = (key) => {
+    swal({
+      title: 'Your conexao will be deleted, do you want to continue?',
+      text: "You won't be able to revert this!",
+      type: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!',
+      cancelButtonText: 'No, cancel!',
+      confirmButtonClass: 'btn btn-success',
+      cancelButtonClass: 'btn btn-danger',
+      buttonsStyling: false
+    }).then(() => {
+      storage.remove(key)
+      swal('Deleted!', 'Your conexao has been deleted.','success')
+    }, (dismiss) => {
+    if (dismiss === 'cancel')
+      swal('Cancelled','Your conexao is safe :)','error')
+    })
+  }
 }
 angular
 .module('geogig-desktop')
